fff8d98fbd95f5a208d9b49f70b1943cbfc75cb5
hady <osmanhady73@gmail.com>
2024-10-11 18:23:32 +0300

    Add template file for preserve animation between pages

---
+++ src/app/template.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React, { ReactNode } from "react";
+import { motion } from "framer-motion";
+
+const RootTemplate = ({ children }: { children: ReactNode }) => {
+  return (
+    <motion.main
+      transition={{ duration: 0.4 }}
+      initial={{ opacity: 0.3, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0.3, y: 20 }}
+      className="grid flex-grow bg-slate-200/70"
+    >
+      {children}
+    </motion.main>
+  );
+};
+
+export default RootTemplate;
